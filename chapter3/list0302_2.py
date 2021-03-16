8b8b230dda464b20af43542ba0edeb0eef980bcd
ryusei <assaultaim@yahoo.co.jp>
2021-03-16 19:01:55 +0900

    Initial commit

---
+++ chapter3/list0302_2.py
@@ -0,0 +1,5 @@
+life = 0
+if life <= 0:
+    print("ゲームオーバー")
+if life > 0:
+    print("ゲーム続行！")
---
+++ test3.py
@@ -0,0 +1,7 @@
+print("俺といえば？") #コメントアウト！！
+name = input()
+print("俺は" + name + "！！")
+
+print("セフィロスと言えば？")
+name = input()
+print(name + "!!")
